3ed558e87632c124709e55f281c8882f4a29905a
Aaron Smith <smitham50@gmail.com>
2019-07-09 17:22:33 -0400

    added to users controller and commented out

--- yard-sale-api/app/controllers/api/v1/users_controller.rb
+++ yard-sale-api/app/controllers/api/v1/users_controller.rb
@@ -7,8 +7,9 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def create 
+        # results = Geocoder.search(params[:location])
         @user = User.find_or_create_by(id: params[:id],name: params[:name],location: params[:location], email: params[:email])
-
+        # @user.update(long: results.first.coordinates[0], lat: results.first.coordinates[1])
         render json: @user
     end
 end
