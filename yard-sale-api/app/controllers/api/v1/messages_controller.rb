8c38f47ee03863dd902de3433528a438fc46d8af
github username <github email address>
2019-07-09 15:00:01 -0400

    message func

--- yard-sale-api/app/controllers/api/v1/messages_controller.rb
+++ yard-sale-api/app/controllers/api/v1/messages_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::MessagesController < ApplicationController
+  def index
+      @messages = Message.all
+
+      render json: @messages
+  end
+
+  def create
+    # byebug
+      @message = Message.create(body: params[:body], user_id: params[:user_id], item_id: params[:item_id])
+
+      render json: @message
+  end
+
 end
