2aac8320c434d3b333d36631cbee54cc758cc1ac
Stephen Brewer <stephebrewer16@gmail.com>
2019-07-11 11:30:27 -0400

    browse y category

--- yard-sale-api/config/routes.rb
+++ yard-sale-api/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do
+      get "/items/:category", to: "items#items_by_category"
       resources :users
       resources :items
       resources :messages
